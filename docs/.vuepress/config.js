0c6bd78a168dc388ed6aa870e41d60f2232e45fd
teslapeer <zxz9457639@gmail.com>
2023-10-08 21:34:13 +0800

    deploy reco commit

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,8 +1,49 @@
 import { defineUserConfig } from 'vuepress'
-
+import { recoTheme } from 'vuepress-theme-reco'
 export default defineUserConfig({
   lang: 'zh-CN',
   logo:'/images/hero.png',
   title: 'teslapeerBlog',
   description: '这是我的第一个 VuePress 站点',
+  theme: recoTheme({
+    author: 作者昵称,
+    authorAvatar: '/images/hero.png',
+    navbar : [
+      { text: '指南', link: '/docs/docs/系列1/分组1-1/笔记1', icon: 'Compass' },
+      { 
+        text: '笔记',
+        icon: 'Document',
+        children: [
+          {
+            text: '系列1',
+            children: [
+              { text: '分组1-1', link: '/docs/docs/系列1/分组1-1' },
+              { text: '分组1-2', link: '/docs/docs/系列1/分组1-2' }
+            ],
+          },
+          {
+            text: '系列2',
+            children: [
+              { text: '分组2-1', link: '/docs/docs/系列2/分组2-1' },
+              { text: '分组2-2', link: '/docs/docs/系列2/分组2-2' }
+            ],
+          },
+        ],
+      
+      }
+    ],
+    // 自动设置blogs分类
+    autoSetBlogCategories: true,
+    // 自动将分类和标签添加至头部导航条
+    autoAddCategoryToNavbar: {
+      location: 1, // 默认 0
+      categoryText: '分类', // 默认 categories
+      tagText: '标签' // 默认 tags
+    },
+    // 当 autoAddCategoryToNavbar 为 true 时，则全部取默认值
+    autoAddCategoryToNavbar: true,
+    // 自动设置docs分类
+    autoSetSeries: true,
+    catalogTitle: '本页目录'
+  })
 })
--- docs/README.md
+++ docs/README.md
@@ -1,6 +1,38 @@
 ---
-lang: zh-CN
-title: 页面的标题
-description: 页面的描述
+home: true
+modules: # 指定首页展示模块
+  - Banner
+  - BannerBrand
+  - Blog
+  - MdContent
+  - Footer
+banner: # banner 模块的配置
+  heroText: teslapeer
+  tagline: 日常杂碎、学习笔记、个人知识库。
+  heroImage: /imags/hero.png
+  heroImageStyle:
+    maxWidth: 200px
+    margin: 0 auto 2rem
+  bgImage: /imags/bg.svg
+  bgImageStyle: 
+    height: 450px
+
+bannerBrand: # bannerBrand 模块的配置
+  title: teslapeer's Blogs & Notes
+  description: 为天地立心，为生民立命，为往圣继绝学，为万世开太平
+  tagline: 日常杂碎、学习笔记、个人知识库。
+
+blog: # blog 模块的配置
+  socialLinks: # 社交地址
+    - { icon: 'LogoGithub', link: 'https://github.com/TeslaPeer' },
+    - { icon: 'Email', link: 'https://github.com/TeslaPeer' },
+    - { icon: 'LogoWechat', link: 'https://github.com/TeslaPeer' }
+
+footer: # 底部模块的配置
+  record: 域名备案文案
+  recordLink: 域名备案地址
+  cyberSecurityRecord: 公安备案文案
+  cyberSecurityLink: 公安备案地址
+  startYear: 2023
 ---
-# Hello VuePress
+
