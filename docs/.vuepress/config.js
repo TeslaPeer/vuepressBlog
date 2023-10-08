5208f4d735c427b5842767092c24904f9cebce24
teslapeer <zxz9457639@gmail.com>
2023-10-08 14:37:59 +0800

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,8 @@
+import { defineUserConfig } from 'vuepress'
+
+export default defineUserConfig({
+  lang: 'zh-CN',
+  logo:'/images/hero.png',
+  title: 'teslapeerBlog',
+  description: '这是我的第一个 VuePress 站点',
+})
